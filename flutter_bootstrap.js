8e9747a7cde8ec868deed072e08eea8b094fa4c7
Felipe Crochik <felipe@crochik.com>
2026-06-03 12:46:19 -0400

    strike 2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4046666487" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4260960853" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
